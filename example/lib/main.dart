2b164e1887c3b253880553a4ed95c601d503a9f9
Georgios Chatoutsidis <giorgoshatoutsidis@gmail.com>
2021-09-23 00:41:38 +0300

    disabled broken code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,8 +30,7 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await Flgl.platformVersion ?? 'Unknown platform version';
+      // platformVersion = await Flgl.platformVersion ?? 'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -42,7 +41,7 @@ class _MyAppState extends State<MyApp> {
     if (!mounted) return;
 
     setState(() {
-      _platformVersion = platformVersion;
+      // _platformVersion = platformVersion;
     });
   }
 
