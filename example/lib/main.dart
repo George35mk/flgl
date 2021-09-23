dc186f7e207cd26fd159d98f371b2724b42ef1ed
Georgios Chatoutsidis <giorgoshatoutsidis@gmail.com>
2021-09-23 03:11:19 +0300

    removed unused imports

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flgl_example/home.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-import 'package:flgl/flgl.dart';
 
 void main() {
   runApp(const MyApp());
