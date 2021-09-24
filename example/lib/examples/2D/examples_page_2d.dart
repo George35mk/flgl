f2d081ee47f71c723790ac5805a9982d1aa864a5
Georgios Chatoutsidis <giorgoshatoutsidis@gmail.com>
2021-09-24 03:19:44 +0300

    changed the map keys

--- example/lib/examples/2D/examples_page_2d.dart
+++ example/lib/examples/2D/examples_page_2d.dart
@@ -9,16 +9,16 @@ class ExamplesPage2d extends StatefulWidget {
 
 class _ExamplesPage2dState extends State<ExamplesPage2d> {
   final Map<String, dynamic> _pages = {
-    '9': {"name": "Example 9", "description": "OpenGLES: 2D Translation", 'route': 'example_9'},
-    '10': {"name": "Example 10", "description": "OpenGLES: 2D Rotation", 'route': 'example_10'},
-    '11': {"name": "Example 11", "description": "OpenGLES: 2D Scale", 'route': 'example_11'},
-    '12': {"name": "Example 12", "description": "OpenGLES: 2D Matrices", 'route': 'example_12'},
-    '13': {
+    '1': {"name": "Example 9", "description": "OpenGLES: 2D Translation", 'route': 'example_9'},
+    '2': {"name": "Example 10", "description": "OpenGLES: 2D Rotation", 'route': 'example_10'},
+    '3': {"name": "Example 11", "description": "OpenGLES: 2D Scale", 'route': 'example_11'},
+    '4': {"name": "Example 12", "description": "OpenGLES: 2D Matrices", 'route': 'example_12'},
+    '5': {
       "name": "Example 13",
       "description": "OpenGLES: 2D Matrices reducing complexity",
       'route': 'example_13'
     },
-    '14': {
+    '6': {
       "name": "Example 14",
       "description": "OpenGLES: 2D Matrices Improving the matrix",
       'route': 'example_14'
