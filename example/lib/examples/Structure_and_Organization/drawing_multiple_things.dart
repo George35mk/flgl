e189f65ab775429c10877a8b888393ec3561f198
Georgios Chatoutsidis <giorgoshatoutsidis@gmail.com>
2021-10-01 16:00:49 +0300

    unused code removed
    - also moved the js example inside a folder

--- example/lib/examples/Structure_and_Organization/drawing_multiple_things.dart
+++ example/lib/examples/Structure_and_Organization/drawing_multiple_things.dart
@@ -24,20 +24,6 @@ class DrawingMultipleThings1 extends StatefulWidget {
 class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
   bool initialized = false;
 
-  dynamic positionLocation;
-  dynamic normalLocation;
-  dynamic worldViewProjectionLocation;
-  dynamic worldInverseTransposeLocation;
-  dynamic worldLocation;
-  dynamic colorLocation;
-  dynamic shininessLocation;
-  dynamic lightDirectionLocation;
-  dynamic innerLimitLocation;
-  dynamic outerLimitLocation;
-  dynamic lightWorldPositionLocation;
-  dynamic viewWorldPositionLocation;
-  dynamic positionBuffer;
-  dynamic normalBuffer;
   dynamic program;
 
   late Flgl flgl;
@@ -51,17 +37,9 @@ class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
 
   // double fieldOfViewRadians = 60.0;
   double fRotationRadians = 0.0;
-  double shininess = 150.0;
-  double lightRotationX = 0.0;
-  double lightRotationY = 0.0;
-  List<double> lightDirection = [0.0, 0.0, 1.0]; // this is computed in updateScene
-  double innerLimit = 10.0;
-  double outerLimit = 20.0;
 
   double time = 0;
-  double cameraAngleRadians = 0;
   double fieldOfViewRadians = 60;
-  double cameraHeight = 50;
 
   // Uniforms for each object.
   var sphereUniforms = {
@@ -81,31 +59,32 @@ class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
   var coneTranslation = [40, 0, 0];
 
   computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation) {
-    var matrix = M4.translate(viewProjectionMatrix, translation[0], translation[1], translation[2]);
+    var matrix = M4.translate(
+      viewProjectionMatrix,
+      translation[0],
+      translation[1],
+      translation[2],
+    );
     matrix = M4.xRotate(matrix, xRotation);
     return M4.yRotate(matrix, yRotation);
   }
 
+  Timer? timer;
+  dynamic programInfo;
+
   dynamic sphereBufferInfo;
   dynamic cubeBufferInfo;
   dynamic coneBufferInfo;
-  dynamic programInfo;
 
   TransformControlsManager? controlsManager;
-  Timer? timer;
 
   @override
   void initState() {
     super.initState();
 
     // init control manager.
-    // ! add more controls for scale and rotation.
     controlsManager = TransformControlsManager({});
     controlsManager!.add(TransformControl(name: 'fRotation', min: -360, max: 360, value: 0.0));
-    // controlsManager!.add(TransformControl(name: 'lightRotationX', min: -2, max: 2, value: lightRotationX));
-    // controlsManager!.add(TransformControl(name: 'lightRotationy', min: -2, max: 2, value: lightRotationY));
-    // controlsManager!.add(TransformControl(name: 'innerLimit', min: 0, max: 180, value: innerLimit));
-    // controlsManager!.add(TransformControl(name: 'outerLimit', min: 0, max: 180, value: outerLimit));
   }
 
   @override
@@ -135,7 +114,8 @@ class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
 
                     initGl();
                     draw();
-                    timer = Timer.periodic(Duration(milliseconds: 20), (Timer t) => {draw()});
+                    // Draw 50 frames per second.
+                    timer = Timer.periodic(const Duration(milliseconds: 20), (Timer t) => {draw()});
                   });
                 },
               ),
@@ -151,18 +131,6 @@ class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
                       switch (control.name) {
                         case 'fRotation':
                           fRotationRadians = MathUtils.degToRad(control.value);
-                          break;
-                        case 'lightRotationX':
-                          lightRotationX = control.value;
-                          break;
-                        case 'lightRotationy':
-                          lightRotationY = control.value;
-                          break;
-                        case 'innerLimit':
-                          innerLimit = control.value;
-                          break;
-                        case 'outerLimit':
-                          outerLimit = control.value;
                           break;
                         default:
                       }
@@ -201,7 +169,7 @@ class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
   }
 
   draw() {
-    time += 0.05;
+    time += 0.01;
 
     // Tell WebGL how to convert from clip space to pixels
     gl.viewport(0, 0, (width * flgl.dpr).toInt(), (height * flgl.dpr).toInt());
@@ -215,9 +183,6 @@ class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
     // Turn on culling. By default backfacing triangles will be culled.
     gl.enable(gl.CULL_FACE);
     gl.enable(gl.DEPTH_TEST);
-
-    // Tell it to use our program (pair of shaders)
-    // gl.useProgram(program);
 
     // ----------------------- Matrix setup-----------------------
 
@@ -246,6 +211,7 @@ class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
     var coneXRotation = time;
     var coneYRotation = -time;
 
+    // Tell it to use our program (pair of shaders)
     gl.useProgram(programInfo['program']);
 
     // ------ Draw the sphere --------
@@ -298,12 +264,6 @@ class _DrawingMultipleThings1State extends State<DrawingMultipleThings1> {
     BFX.setUniforms(programInfo, coneUniforms);
 
     gl.drawArrays(gl.TRIANGLES, 0, coneBufferInfo['numElements']);
-
-    // // Draw the geometry.
-    // var primitiveType = gl.TRIANGLES;
-    // var offset_ = 0;
-    // var count = 16 * 6;
-    // gl.drawArrays(primitiveType, offset_, count);
 
     // !super important.
     gl.finish();
