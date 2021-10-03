7808763d6d2d6b998931c124699bb6bba95a554c
Georgios Chatoutsidis <giorgoshatoutsidis@gmail.com>
2021-10-03 16:32:03 +0300

    extract class ArrayBuffer

--- example/lib/bfx/primitives.dart
+++ example/lib/bfx/primitives.dart
@@ -2,48 +2,7 @@ import 'dart:math';
 import 'dart:typed_data';
 import 'package:flgl/openGL/contexts/open_gl_context_es.dart';
 
-class ArrayBuffer {
-  /// The number of [components].
-  int numComponents = 0;
-
-  /// The number of vertices.
-  int numVertices = 0;
-
-  /// The list type
-  /// Supported types are:
-  /// - Float32
-  /// - Uint16
-  /// - Uint8
-  String type = '';
-
-  /// The Array buffer data list.
-  dynamic data;
-  int cursor = 0;
-
-  ArrayBuffer(this.numComponents, this.numVertices, this.type) {
-    if (type == 'Float32') {
-      data = Float32List(numComponents * numVertices);
-    } else if (type == 'Uint16') {
-      data = Uint16List(numComponents * numVertices);
-    } else if (type == 'Uint8') {
-      data = Uint8List(numComponents * numVertices);
-    } else {
-      throw 'Unsupported list type';
-    }
-  }
-
-  /// Returns the number of elements.
-  get numElements {
-    return (data.length ~/ numComponents).toInt() | 0;
-  }
-
-  push(List<num> args) {
-    for (var i = 0; i < args.length; i++) {
-      var value = args[i];
-      data[cursor++] = value;
-    }
-  }
-}
+import 'array_buffer.dart';
 
 ///  Array of the indices of corners of each face of a cube.
 ///  @type {Array.<number[]>}
