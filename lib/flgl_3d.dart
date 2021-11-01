fa71a5553115b220312f0464632b9e9fa9c9f545
george35mk <george35mk@gmail.com>
2021-11-02 01:39:12 +0200

    moved mesh inside objects dir

--- lib/flgl_3d.dart
+++ lib/flgl_3d.dart
@@ -8,10 +8,11 @@ export './flutter3d/cameras/perspective_camera.dart';
 
 export './flutter3d/core/buffer_attribute.dart';
 export './flutter3d/core/buffer_geometry.dart';
-export './flutter3d/core/mesh.dart';
 export './flutter3d/core/object_3d.dart';
 export './flutter3d/core/programs.dart';
 export './flutter3d/core/renderer.dart';
+
+export './flutter3d/objects/mesh.dart';
 
 export './flutter3d/geometries/plane_geometry.dart';
 export './flutter3d/geometries/triangle_geometry.dart';
--- lib/flutter3d/objects/mesh.dart
+++ lib/flutter3d/objects/mesh.dart
@@ -1,8 +1,8 @@
 import 'package:flgl/flutter3d/materials/material.dart';
 import 'package:flgl/openGL/contexts/open_gl_context_es.dart';
 
-import 'buffer_geometry.dart';
-import 'object_3d.dart';
+import '../core/buffer_geometry.dart';
+import '../core/object_3d.dart';
 
 class Mesh extends Object3D {
   Mesh(OpenGLContextES gl, BufferGeometry geometry, Material material) : super(gl, geometry, material);
