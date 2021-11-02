37e3dc191bbbf62e2e4f4accf25c9f18f237a0d5
george35mk <george35mk@gmail.com>
2021-11-02 13:44:08 +0200

    added new properties in material and meshBasicMaterial classes

--- lib/flutter3d/materials/mesh_basic_material.dart
+++ lib/flutter3d/materials/mesh_basic_material.dart
@@ -2,7 +2,15 @@ import 'package:flgl/flutter3d/materials/material.dart';
 import 'package:flgl/flutter3d/math/color.dart';
 
 class MeshBasicMaterial extends Material {
+  /// The material color.
   Color color = Color(1, 1, 1, 1);
+
+  /// If set to true then the program will chose
+  /// `gl.LINES` instead of `gl.TRIANGES`.
+  bool wireframe = false;
+
+  /// The texture map.
+  dynamic map; // the texture map.
 
   MeshBasicMaterial({Color? color}) {
     this.color = color ?? Color(1, 1, 1, 1);
